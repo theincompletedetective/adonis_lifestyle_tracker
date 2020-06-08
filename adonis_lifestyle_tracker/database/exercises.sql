bf6e0fe6ffef230c18e2b0f6260b93315232d26e
Elijah Smith <esmith1412@posteo.net>
2020-06-07 20:13:59 -0400

    Added SQL database scheme for exercises database

---
+++ adonis_lifestyle_tracker/database/exercises.sql
@@ -0,0 +1,17 @@
+create table exercise (
+    id integer primary key,
+    exercise_name text,
+    reps_5 text,
+    reps_8 text,
+    reps_13 text,
+    reps_21 text
+);
+
+create table week (
+    id integer primary key
+);
+
+create table exercise_week (
+    exercise_id integer,
+    week_id integer
+);
