f59a48e3c6516dea03f9a471f709162ceb32deb3
Elijah Smith <esmith1412@posteo.net>
2020-06-24 12:44:42 -0400

    Changed size of input fields in add_week module to come from COnfig object

--- adonis_lifestyle_tracker/nutrition/add_week.py
+++ adonis_lifestyle_tracker/nutrition/add_week.py
@@ -1,21 +1,17 @@
 ''''Adds a week's total calories and protein to the nutrition database.'''
-
 import PySimpleGUI as sg
+from adonis_lifestyle_tracker.config import Config
 from adonis_lifestyle_tracker.gui.confirmation_gui import get_confirmation
 from adonis_lifestyle_tracker.nutrition.nutrition import add_week
-
-
-TEXT_SIZE = (20, 1)
-INPUT_SIZE = (10, 1)
 
 
 sg.theme('Reddit')
 
 layout = [
-    [sg.Text('Week Number', size=TEXT_SIZE), sg.Input(key='-WEEK-', size=INPUT_SIZE)],
-    [sg.Text('Total Calories (kcal)', size=TEXT_SIZE), sg.Input(key='-CALORIES-', size=INPUT_SIZE)],
-    [sg.Text('Total Protein (g)', size=TEXT_SIZE), sg.Input(key='-PROTEIN-', size=INPUT_SIZE)],
-    [sg.Submit(button_color=('white', '#008000')), sg.Cancel( button_color=('black', '#ff0000') )]
+    [sg.Text('Week Number', size=Config.LABEL_SIZE), sg.Input(key='-WEEK-', size=Config.NUMBER_SIZE)],
+    [sg.Text('Total Calories (kcal)', size=Config.LABEL_SIZE), sg.Input(key='-CALORIES-', size=Config.NUMBER_SIZE)],
+    [sg.Text('Total Protein (g)', size=Config.LABEL_SIZE), sg.Input(key='-PROTEIN-', size=Config.NUMBER_SIZE)],
+    [sg.Submit(button_color=Config.SUBMIT_BUTTON_COLOR), sg.Cancel(button_color=Config.CANCEL_BUTTON_COLOR)]
 ]
 
 window = sg.Window('Add Week GUI', layout)
