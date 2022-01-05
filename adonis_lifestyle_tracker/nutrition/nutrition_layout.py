7f10ef862a97584d90470100bf789a79fef8dcda
Elijah Smith <esmith6@columbus.k12.oh.us>
2022-01-05 17:54:56 -0500

    Increase length of food input combo box

--- adonis_lifestyle_tracker/nutrition/nutrition_layout.py
+++ adonis_lifestyle_tracker/nutrition/nutrition_layout.py
@@ -8,7 +8,7 @@ NUTRITION_LABEL_SIZE = (7, 1)
 EXERCISE_LABEL_SIZE = (10, 1)
 TEXT_INPUT_SIZE = (42, 1)
 NUM_INPUT_SIZE = (6, 1)
-BUTTON_SIZE = (16, 1)
+BUTTON_SIZE = (25, 1)
 
 ADD_BUTTON_COLOR = ('white', '#008000')
 CHANGE_BUTTON_COLOR = ('black', '#ffd700')
@@ -21,7 +21,7 @@ layout = [
     ],
     [
         sg.T('Food', size=NUTRITION_LABEL_SIZE),
-        sg.InputCombo(tuple(), key='-FOOD-', font=('Any', 9), size=(30, 1)),
+        sg.InputCombo(tuple(), key='-FOOD-', font=('Any', 9), size=(55, 1)),
         sg.T('Quantity'),
         sg.InputCombo([x + 1 for x in range(20)], key='-QUANTITY-', font=('Any', 9), size=(3, 1), default_value=1),
     ],
@@ -110,7 +110,7 @@ layout = [
     ],
     [sg.Frame('Database Path', layout=[
         [
-            sg.I(key='-PATH-', enable_events=True, size=(32, 1)),
+            sg.I(key='-PATH-', enable_events=True, size=(58, 1)),
             sg.FileBrowse(),
             sg.Button('Load Database', button_color=('white', '#8a2be2'))
         ]
